52bd88c6cce2c4006ca6b1d0cc2b3d2343a31da0
Niv-1993 <nivdan7@gmail.com>
2023-08-14 23:20:46 +0300

    init springboot app

---
+++ sponsored-ads/src/main/java/criteo/sponsoredads/SponsoredAdsApplication.java
@@ -0,0 +1,13 @@
+package criteo.sponsoredads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SponsoredAdsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SponsoredAdsApplication.class, args);
+    }
+
+}
---
+++ sponsored-ads/src/test/java/criteo/sponsoredads/SponsoredAdsApplicationTests.java
@@ -0,0 +1,13 @@
+package criteo.sponsoredads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SponsoredAdsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
